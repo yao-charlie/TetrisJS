ae90ecddb84dbfbe82de873e34592ea4496f51ae
yao-charlie <charlieyao@gmail.com>
2020-12-07 02:10:29 -0800

    Left, right, down control implemented. Improved timing for freezing.

--- tetris_engine.js
+++ tetris_engine.js
@@ -67,18 +67,18 @@ let currentRotation = 0
 
 //randolmly select Tetramino
 let random = Math.floor(Math.random()*Tetraminoes.length)
-let current = Tetraminoes[random][currentRotation]
+let currentPiece = Tetraminoes[random][currentRotation]
 
 //draw Tetramino as a map to CSS style.
 function draw(){
-  current.forEach(index =>{
+  currentPiece.forEach(index =>{
     squares[currentPosition+index].classList.add('tetramino')
   })
 }
 
 //undraw Tetramino
 function undraw(){
-  current.forEach(index=>{
+  currentPiece.forEach(index=>{
     squares[currentPosition+index].classList.remove('tetramino')
   })
 }
@@ -87,29 +87,121 @@ draw()
 
 // tetramino falling
 
-var interval = 500; //timer in ms to start
+var interval = 200 //timer in ms to start
 timer = setInterval(moveDown, interval)
 
+
+//TODO: Collision bug... moveLeft/moveRight fires while moveDown is activating,
+//      allowing tetramino to mome to adjacent block before logic checks to freeze.
+//      "suspending" EventListner does not seem to solve this...
+
 function moveDown(){
+  document.removeEventListener('keydown',control)
   undraw()
   currentPosition += width
   draw()
-  freeze()
+
+  if(freezeCheck()){
+    clearInterval(timer)
+    setTimeout(()=>{
+      freeze()
+      timer=setInterval(moveDown,interval)
+    },interval*2)
+  }
+
+  // freeze()deprecated. Above added to delay freeze to allow 'final' movements before freezing.
+
+  document.addEventListener('keydown',control)
 }
 
+//About to collide check
 
-//freeze tetramino
+function freezeCheck(){
+  return currentPiece.some( index=> squares[currentPosition + index + width].classList.contains('taken'))
+}
+
+// freeze tetramino
 function freeze(){
-  if(current.some( index=> squares[currentPosition + index + width].classList.contains('taken'))){
-    current.forEach(index=> squares[currentPosition + index].classList.add('taken'))
+  if(freezeCheck()){
+
+
+    currentPiece.forEach(index=> squares[currentPosition + index].classList.add('taken'))
     //spawn a new Tetramino
     random = Math.floor(Math.random()*Tetraminoes.length)
-    current = Tetraminoes[random][currentRotation]
+    currentPiece = Tetraminoes[random][currentRotation], interval
     currentPosition = 4
     draw()
   }
 }
 
 
+// function freeze(){
+//   if(currentPiece.some( index=> squares[currentPosition + index + width].classList.contains('taken'))){
+//     clearInterval(timer)
+//     setTimeout( ()=>{
+//       timer = setInterval(moveDown, interval)
+//       currentPiece.forEach(index=> squares[currentPosition + index].classList.add('taken'))
+//
+//       //spawn a new Tetramino
+//       random = Math.floor(Math.random()*Tetraminoes.length)
+//       currentPiece = Tetraminoes[random][currentRotation], interval
+//       currentPosition = 4
+//       draw()
+//
+//     }, 500)
+//   }
+// }
+
+//moving Tetramino
+
+// Alternate move:
+// function moveLeft() {
+//   undraw()
+//   const isAtLeftEdge = currentPiece.some(index => (currentPosition + index) % width === 0)
+//   if(!isAtLeftEdge) currentPosition -= 1
+//   if(currentPiece.some(index => squares[currentPosition + index].classList.contains('taken'))){
+//     currentPosition += 1
+//   }
+//   draw()
+// }
+
+
+
+
+function moveLeft() {
+  const isAtLeftEdge = currentPiece.some(index => (currentPosition + index) % width === 0)
+  if(isAtLeftEdge) return
+  if(currentPiece.some(index => squares[currentPosition + index-1].classList.contains('taken'))) return
+  undraw()
+  currentPosition-=1
+  draw()
+}
+
+function moveRight() {
+  const isAtRightEdge = currentPiece.some(index => (currentPosition + index) % width === 9)
+  if(isAtRightEdge) return
+  if(currentPiece.some(index => squares[currentPosition + index+1].classList.contains('taken'))) return
+  undraw()
+  currentPosition += 1
+  draw()
+}
+
+
+//keys to movement
+
+function control(key){
+  if (key.keyCode === 37){
+    moveLeft()
+    }
+  if (key.keyCode === 39){
+    moveRight()
+  }
+  if (key.keyCode === 40){
+    if(!freezeCheck()) moveDown() //freezeCheck() needed to prevent move firing before the moveDown freezeCheck fires.
+  }
+
+}
+
+document.addEventListener('keydown', control)
 
 })
