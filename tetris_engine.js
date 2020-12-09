c1201f934f51d2f0b97e27773768d35c85301e8b
yao-charlie <charlieyao@gmail.com>
2020-12-09 14:47:00 -0800

    Revised implementation of rotate. No kick but no wrap.

--- tetris_engine.js
+++ tetris_engine.js
@@ -5,6 +5,8 @@ let squares = Array.from(document.querySelectorAll('.grid div'))
 const DisplayScore = document.querySelector('#score')
 const StartButton = document.querySelector('#start__button')
 const width = 10
+const height = 20
+var score = 0
 
 
 //Tetraminoes
@@ -28,7 +30,7 @@ const RTetratmino = [
     // [width, width+1, width]
   ]
 
-  const TTetratmino = [
+const TTetratmino = [
     [width, width+1, width+2, width*2+1],
     [2, width+1, width+2, width*2+2],
     [width+1, width*2, width *2+1, width*2+2],
@@ -96,44 +98,94 @@ var interval = 500 //timer in ms to start
 var timer = setInterval(moveDown, interval)
 
 
+
 //Collision Detection
 
 //bottom/pieces
 function freezeCheck(){
-  return currentPiece.some( index=> squares[currentPosition + index + width].classList.contains('taken'))
+  return currentPiece.some( index => squares[currentPosition + index + width].classList.contains('taken'))
 }
 
 
 function leftCheck(){
-  if(currentPiece.some(index => (currentPosition + index) % width === 0)) return true //wall
-  if(currentPiece.some( index=> squares[currentPosition + index - 1].classList.contains('taken'))) return true //piece
+  if(currentPiece.some( index => (currentPosition + index) % width === 0)) return true //wall
+  if(currentPiece.some( index => squares[currentPosition + index - 1].classList.contains('taken'))) return true //piece
   return false
 }
 
 function rightCheck(){
-  if(currentPiece.some(index => (currentPosition + index) % width === width - 1)) return true //wall
-  if(currentPiece.some( index=> squares[currentPosition + index + 1].classList.contains('taken'))) return true //piece
+  if(currentPiece.some( index => (currentPosition + index) % width === width - 1)) return true //wall
+  if(currentPiece.some( index => squares[currentPosition + index + 1].classList.contains('taken'))) return true //piece
   return false
 }
 
+// function rightSide(){
+//   if(currentPiece.every(index => (currentPosition + index) % width > 5) return true
+//   return false
+// }
+//
+// function leftSide(){
+//   if(currentPiece.every(index => (currentPosition + index) % width < 5) return true
+//   return false
+// }
+
+//Broken code to be revisited:
+
+// function rowCompleted(currentRow){
+//   for(k=0; k<width; k++){
+//     console.log("k is " + k)
+//     var foo = k=>squares[k+currentRow*width].classList.contains('taken')
+//     console.log(foo)
+//     // if(!(currentRow=>squares[checkIndex+j+currentRow*width].classList.contains('taken'))) return false
+//   }
+//   return true
+// }
+//
+// // Scoring
+//
+// function scoreUp(){
+//   // console.log("current Postion is" + currentPosition)
+//   let checkIndex = currentPosition - currentPosition%width
+//   // console.log("check index is " + checkIndex)
+//   let rowsCompleted = 0
+//   let pieceScore = 0
+//
+//   for(i = 0; i<6 ; i++){ //4 is max number of rows possible to make or should be but I seems bugged
+//     if (i * width + checkIndex > width * height) {break}
+//     if (rowCompleted(checkIndex)) {
+//       pieceScore++
+//       // console.log(pieceScore)
+//     }
+//   }
+//
+// }
 
 
-// freeze tetramino
+
+
+
+
+// Freeze tetramino
 function freeze(){
   if(freezeCheck()){
 
     currentPiece.forEach(index=> squares[currentPosition + index].classList.add('taken'))
+
+    // scoreUp()
     //spawn a new Tetramino
     random = Math.floor(Math.random()*Tetraminoes.length)
     currentPiece = Tetraminoes[random][currentRotation]
     currentPosition = 4
     draw()
+
   }
 }
 
 
 
-//moving Tetramino
+
+
+//Moving Tetramino
 
 // Alternate move:
 // function moveLeft() {
@@ -148,7 +200,6 @@ function freeze(){
 
 function moveDown(){
   document.removeEventListener('keydown',control)
-
   if(freezeCheck()){
     clearInterval(timer)
     setTimeout(()=>{
@@ -157,17 +208,14 @@ function moveDown(){
     },interval*2) //times 2 for more 'natural' feel
     return
   }
-
   undraw()
   currentPosition += width
   draw()
   //might use for moveLeft/Right but seems to still be bugged.
-
   document.addEventListener('keydown',control)
 }
 
 function moveLeft() {
-
   if(leftCheck()) return;
   undraw()
   currentPosition--
@@ -175,40 +223,53 @@ function moveLeft() {
 }
 
 function moveRight() {
-
   if(rightCheck()) return;
   undraw()
   currentPosition++
   draw()
-
 }
 
+// function rotate(){
+//   currentRotation++
+//   currentRotation%=4
+//   undraw()
+//   currentPiece = Tetraminoes[random][currentRotation]
+//   //N.B. This might introduce 'wall kick' behaviour.
+//   if(rightCheck()){
+//     while(rightCheck()) {
+//       currentPosition--
+//     }
+//     currentPosition++ //needed since rightCheck will cause this loop to always run once
+//   }
+//   else if(leftCheck()){
+//     while(leftCheck()) {
+//       currentPosition++
+//     }
+//     currentPosition-- //needed since leftCheck will cause this loop to always run once
+//   }
+//   draw()
+// }
+
+//no kick
 function rotate(){
+  placeholderPiece = currentPiece
+  console.log(currentRotation)
   currentRotation++
+  console.log(currentRotation)
   currentRotation%=4
   undraw()
   currentPiece = Tetraminoes[random][currentRotation]
-
-  //N.B. This might introduce 'wall kick' behaviour.
-  if(rightCheck()){
-    while(rightCheck()) {
-      currentPosition--
-    }
-    currentPosition++ //needed since rightCheck will cause this loop to always run once
-  }
-
-  else if(leftCheck()){
-    while(leftCheck()) {
-      currentPosition++
-    }
-    currentPosition-- //needed since leftCheck will cause this loop to always run once
+  if(rightCheck() && leftCheck() ){
+    currentRotation--
+    console.log("hit side and " + currentRotation)
+    currentRotation+=width
+    currentRotation%=4
+    currentPiece = placeholderPiece
   }
   draw()
 }
 
-
-
-//keys to movement
+//Keys to movement
 
 function control(key){
   if (key.keyCode === 37){
